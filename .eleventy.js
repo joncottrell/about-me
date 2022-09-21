732d4621c9a13885b37fe2aa865cd93a934c3e54
Jonathan Cottrell <cottrell.jonathan@gmail.com>
2022-09-21 10:36:34 -0700

    Fix missing _site

--- .eleventy.js
+++ .eleventy.js
@@ -4,4 +4,12 @@ module.exports = (config) => {
   config.addNunjucksShortcode("FontAwesomeIcon", FontAwesomeIcon);
   config.addPassthroughCopy("css");
   config.addPassthroughCopy("images");
+
+  return {
+    dir: {
+      input: ".",
+      output: "_site",
+    },
+    passthroughFileCopy: true,
+  };
 };
